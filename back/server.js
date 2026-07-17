42bf537c5426e476ed3c2661945a5d331df72713
AlbertoNguenha <albertodamadalenajoaonguenha@gmail.com>
2026-07-17 04:36:23 +0200

    api concluida

--- back/server.js
+++ back/server.js
@@ -1,14 +1,15 @@
-import express from "express";
+import express, { json } from "express";
 
 const app=express();
 
 app.use(express.json())
 
 let pessoa=[]
+//lista todos os usuarios
 app.get("/pessoa", (req,res)=>{
     res.send(pessoa)
 })
-
+//cria um novo usuario
 app.post("/pessoa", (req,res)=>{
     const {nome,curso, telefone}=req.body
     pessoa.push({nome,curso, telefone})
@@ -16,7 +17,18 @@ app.post("/pessoa", (req,res)=>{
         messagem:"Usuario criado com sucesso"
     })
 })
+//actualiza um usuario
+app.put("/pessoa/:id", (req,res)=>{
+    const id=Number(req.params.id)
+    const i=pessoa.findIndex(u=>u.id==id)
 
+    if(id==-1){
+        return res.status(404).json({messagem:"O usuario nao existe"})
+    }
+
+    pessoa[i]=req.body
+    res,json(pessoa[i])
+})
 
 app.listen(3000, ()=>{
     console.log("Conectado")
